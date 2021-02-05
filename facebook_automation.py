0a3ec29dc28bc89bd5ec5011cb86ada9c3e6a170
Evan Shareef <53369789+Evan-CSE@users.noreply.github.com>
2021-02-05 11:02:56 +0600

    Auto facebook login with auto message

---
+++ facebook_automation.py
@@ -0,0 +1,30 @@
+import time
+from art import *
+import text2art
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys;
+
+
+#Facebook Auto Login With AUto Message sending feature
+
+print(text2art("Script By Evan"));
+driver = webdriver.Chrome();
+driver.get("https://www.facebook.com/messages/t/100009757474012");
+el = driver.find_element_by_id("email");
+user = input("Enter username");
+el.send_keys(user);
+passWord = input("Enter password");
+el = driver.find_element_by_id("pass");
+el.send_keys(passWord);
+el.send_keys(Keys.RETURN);
+print("Loading Complete");
+time.sleep(40);
+print("Sending Messages");
+msg = driver.find_element_by_css_selector("._1mf");
+print("Start");
+for i in range(43):
+	M = input("Enter message");
+	msg.send_keys(M+Keys.RETURN);
+	msg = driver.find_element_by_css_selector("._1mf");
+	
+	
